84b162b80e2626af2015d0d5dfdcd6acad545ee4
EzeAleGuzman <ezealeguzman@gmail.com>
2024-09-23 12:32:39 -0300

    cambio base de datos

--- base.py
+++ base.py
@@ -11,12 +11,38 @@ cur.execute('''
                 nombre TEXT NOT NULL,
                 descripcion TEXT,
                 stock INT DEFAULT 0, 
-                activo BOOLEAN DEFAULT TRUE
+                activo BOOLEAN DEFAULT TRUE,
+                precio REAL NOT NULL, 
+                costo REAL NOT NULL,   
                 CHECK (stock >= 0)
             )
 ''')
 
+cur.execute('''
+            CREATE TABLE IF NOT EXISTS boletas(
+                id_boleta INT BIGINT PRIMARY KEY,
+                fecha DATETIME NOT NULL,
+                total REAL NOT NULL
+                
+            )
+            
+''')
 
+
+cur.execute('''
+            CREATE TABLE IF NOT EXISTS detalle_boleta(
+                id_detalle_boleta  INT BIGINT PRIMARY KEY,
+                id_boleta INT NOT NULL,
+                codigo_producto BIGINT NOT NULL,
+                cantidad INT NOT NULL,
+                subtotal REAL NOT NULL,
+                FOREIGN KEY (id_boleta) REFERENCES boletas(id_boleta),
+                FOREIGN KEY (codigo_producto) REFERENCES productos(codigo)
+                
+            )
+            
+            
+''')
 
 #Confirmo los cambios en la base de datos
 con.commit()
