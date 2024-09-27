0d6cc5d60d0e4f48bfd87a28bc29907679e21a38
sarabe89 <ezealeguzman@gmail.com>
2024-09-27 18:32:51 -0300

    se corrigen algunas funciones

--- negocio.py
+++ negocio.py
@@ -3,11 +3,14 @@ import datetime
 from colorama import Fore
 from beautifultable import BeautifulTable
 
+#Estafuncion crea la coneccion a base de datos
 def conectar():
+    #coneccion a la base sino crea una nueva con ese nombre
     con = sqlite3.connect('productos.db')
+    #inicializo la funcion cursor()
     cur = con.cursor()
+    #retorno la coneccion y el cursor
     return con, cur
-
 
 #Funcion para agregar producto
 def agregar(codigo, nombre, descripcion, precio, costo):
@@ -29,28 +32,24 @@ def agregar(codigo, nombre, descripcion, precio, costo):
         con.commit()
         cur.close()
 
-
+#Funcion Para visualizar Los productos Almacenados en la base de datos
 def verProductos():
     con, cur =conectar()
     cur.execute("SELECT codigo, nombre, descripcion, stock FROM productos WHERE activo =TRUE")
     productos = cur.fetchall()
-    # Si hay productos en la base de datos, los mostramos
+    table = BeautifulTable()
     if productos:
-        # Encabezados de las columnas
-        print(f"{'Codigo':<20}  {'Nombre':<20}  {'Descripcion':<20} {'Stock':<10}")  # Encabezados con formato
-        print("=" * 80)  # Línea divisoria
-
+        table.columns.header = ["Codigo", "nombre", "descripcion", "stock"]
         # Imprimimos los productos
         for producto in productos:
-            codigo, nombre, descripcion, stock = producto
-            print(f" {codigo:<20} {nombre:<20}{descripcion or 'N/A':<20} {stock:<10}")  # Ajustamos el formato de cada fila
-            print("-" * 80)
+            table.rows.append(producto)
+        print(table)
     else:
         print("No hay productos disponibles.")
     cur.close()
     con.close()
 
-
+#funsion que se encarga de buscar por nombre un producto en la base de datos
 def buscarPorNombre(nombre):
     #llamo a la funcion conectar para definir mi conexion y usar la funcion cursor
     con, cur =conectar()
@@ -58,21 +57,18 @@ def buscarPorNombre(nombre):
     cur.execute("Select * from productos where nombre =? ", (nombre, ))
     #si
     producto = cur.fetchone()
-    # Verificar si el producto existe
     if producto:
-        # Encabezados de las columnas
-        print(f"{'Nombre':<20} {'Cantidad':<10}")  # Encabezados con formato
-        print("=" * 30)  # Línea divisoria
-        # Imprimir nombre y cantidad (stock) formateado
-        print(f"{producto[0]:<20} {producto[1]:<10}")
+        table = BeautifulTable()
+        table.columns.header = ["Codigo", "nombre", "descripcion", "stock", "activo", "precio", "costo"]
+        table.rows.append(producto)
+        print(table)
+    # Verificar si el producto existe
     else:
         print("Producto no encontrado.")
     cur.close()
     con.close()
 
 #Funcion para realizar una compra en el sistema
-import datetime
-
 def comprar():
     con, cur = conectar()
     productos_comprados = []
@@ -153,8 +149,7 @@ def comprar():
     con.commit()
     cur.close()
     con.close()
-
-    
+ 
 def crearBoleta():
     con, cur =conectar()
     #Busca en la base de datos la ultima boleta y le suma 1
@@ -171,7 +166,6 @@ def crearBoleta():
     con.close()
     return id_boleta
     
-
 def vender(codigo, stock):
     try:
         con, cur =conectar()
@@ -205,16 +199,19 @@ def eliminarProducto(codigo):
 
 def verificarBajoStock():
     con, cur =conectar()
-    cur.execute("SELECT * FROM productos WHERE stock < 6 AND activo =TRUE ")
+    cur.execute("SELECT codigo, nombre, descripcion, stock FROM productos WHERE stock < 6 AND activo =TRUE ")
     productos = cur.fetchall()
     if productos:
-        # Encabezados de las columnas
-        print(f"{'Nombre':<20} {'Cantidad':<10}")  # Encabezados con formato
-        print("=" * 30)  # Línea divisoria
-        # Imprimir nombre y cantidad (stock) formateado
-        for producto in productos:
-            print(f"{producto[1]:<20} {producto[3]:<10}")
+       crearTabla(productos)
     else:
         print("No hay productos con Baja Cantidad.")
     cur.close()
     con.close()
+
+def crearTabla(productos):
+    table = BeautifulTable()
+    table.columns.header = ["Codigo", "nombre", "descripcion", "stock"]
+    # Imprimimos los productos
+    for producto in productos:
+        table.rows.append(producto)
+    print(table)
